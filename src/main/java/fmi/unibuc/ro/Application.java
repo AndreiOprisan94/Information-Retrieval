514acdd7cc1fa2a41e8575d494e4da2dc7a019c7
Andrei Oprisan <oprisanandreidaniel@gmail.com>
2017-03-11 14:44:17 +0200

    Project first source code commit

---
+++ src/main/java/fmi/unibuc/ro/Application.java
@@ -0,0 +1,8 @@
+package fmi.unibuc.ro;
+
+public class Application {
+    public static void main(String args[]){
+        System.out.println(System.getProperty("indexDir") + " " + System.getProperty("dataDir"));
+        System.out.println("Hello Apache Lucene World!");
+    }
+}
